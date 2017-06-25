8c0cc10dac4d4a239c89e32281908cb21cca625e
Marco Carvalho <marcolucio27@gmail.com>
2017-06-25 16:45:25 -0300

    refactoring and creating functions

--- script.R
+++ script.R
@@ -1,6 +1,7 @@
-#df <- read.table("dataset.txt")
-df <- iris
-plot(iris$Sepal.Length, iris$Sepal.Width)
+df_coord <- read.table("dataset.txt")[,1:2]
+df_info <- read.table("dataset.txt")[,-(1:2)]
+# df <- iris
+# plot(iris$Sepal.Length, iris$Sepal.Width)
 epsilon <- 0.1
 alpha <- 0.5
 
@@ -8,25 +9,14 @@ alpha <- 0.5
 # normalize all columns from the df
 ################################################################################
 
-normalise <- function(x) {
+normalize_column <- function(x) {
   ranx <- range(x, na.rm = T)
   (x - ranx[1]) / diff(ranx)
 }
 
-df <- apply(df, 2, normalise)
-
-################################################################################
-# creating df with df x and y
-################################################################################
-
-df_coord <- data.frame(df[,1:2])
+df_coord <- data.frame(apply(df_coord, 2, normalize_column))
 colnames(df_coord) <- c('x', 'y')
-
-################################################################################
-# creating df with df info
-################################################################################
-
-df_info <- data.frame(df[,-(1:2)])
+df_info <- data.frame(apply(df_info, 2, normalize_column))
 
 ################################################################################
 # step 1 - create the matrix
@@ -38,7 +28,7 @@ l <- matrix(nrow = size, ncol = size)
 # dist(rbind(df_coord, df_coord))
 for(i in 1:size) {
   for(j in 1:size) {
-    l[i,j] <- 1 - sqrt(
+    l[i,j] <- sqrt(
       (df_coord$x[i] - df_coord$x[j])^2 
       + 
       (df_coord$y[i] - df_coord$y[j])^2
@@ -50,20 +40,28 @@ for(i in 1:size) {
 # step 2 - normalize the matrix
 ################################################################################
 
-d <- diag(colSums(l, na.rm = TRUE))
-d_a <- diag(diag(d^-alpha))
-la <- d_a %*% l %*% d_a
+diagonal <- function(matrix) {
+  diag(colSums(matrix, na.rm = TRUE))
+}
+
+normalize <- function(matrix, alpha) {
+  d <- diagonal(matrix)
+  d_a <- diag(diag(d^-alpha))
+  d_a %*% matrix %*% d_a
+}
 
 ################################################################################
 # step 3 - form the normalized matrix
 ################################################################################
 
-da <- diag(colSums(la, na.rm = TRUE))
-m <- solve(da) %*% la
+normalized <- function(matrix) {
+  solve(diagonal(normalize(matrix, alpha))) %*% normalize(matrix, alpha)
+}
 
 ################################################################################
 # step 4 - Compute the k largest eigenvalues and the corresponding eigenvectors
 # http://biom300.weebly.com/eigenvalues-and-eigenvectors-in-r.html
 ################################################################################
 
-y <- eigen(t(m))
+df_eigen <- eigen(t(normalized(l)))
+df_eigen
